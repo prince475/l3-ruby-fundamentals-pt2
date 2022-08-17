73ade07df89335d75566562b1549d0d7993f5d49
Ian Okumu <okumu.otsembo@gmail.com>
2022-08-17 08:18:46 +0300

    first commit

---
+++ main.rb
@@ -0,0 +1,33 @@
+## CONTROL STRUCTURES
+
+# CONDITIONAL STATEMENTS
+# 1.0 if - else
+
+
+# 2.0 unless
+
+
+# 3.0 case - when
+
+
+# LOOPS
+# 4.0 while
+
+
+# 5.0 times
+
+
+## ARRAY METHODS (shovel, push, include?, reverse)
+grades = [99, 57, 87, 90, 35, 20, 66, 78, 18, 100]
+
+
+## HASH METHODS (keys, values, delete)
+student = {
+    name: "Jane Doe",
+    age: 22,
+    email: "jane.doe@mail.com",
+    hasGraduated: false,
+    height: 165.5,
+    carModel: nil
+}
+
